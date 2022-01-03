b92d16e66065b11c18d06ff5fec3754fc7e7c67b
gantoin <gantoin@pm.me>
2022-01-03 09:00:47 +0100

    Init project

---
+++ src/main/java/fr/gantoin/githubcity/GithubcityApplication.java
@@ -0,0 +1,13 @@
+package fr.gantoin.githubcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubcityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/gantoin/githubcity/GithubcityApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gantoin.githubcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
